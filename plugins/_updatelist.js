c17c755de80e6f1f0eea99338a6e67b557ee1b60
debojit16mitra <33363581+debojit16mitra@users.noreply.github.com>
2021-09-29 13:20:21 +0530

    Update _updatelist.js

--- plugins/_updatelist.js
+++ plugins/_updatelist.js
@@ -8,20 +8,20 @@ const {MessageType} = require('@adiwajshing/baileys');
 const Language = require('../language');
 const Lang = Language.getString('scrapers');
 
-const eng = `💠● *UPDATE LIST (29/09/2021)* ●💠\n` + 
-`💠● *Büññy®Bot Version: 1.2.0v* ●💠\n\n` +
-`1. Added (.compli)\n` +
-`2. Added (.insult)\n` +
-`3. Improved (.movie)\n` +
-`4. Bot admins are fixed. (.botmin) If you want to be a bot admin pay 1 Million $ to existing admins.\n` +
-`5. Removed (.currency) its buggy\n` +
-`6. Introduced (.news)\n` +
-`7. Added (.gaymeter)\n` +
-`8. Fixed (.report) Enjoy reporting\n` +
-`9. Inroduced (.device)\n` +
-`10. Sleep mode enhanced for admins to control NSFW\n` +
-`11. Introduced (.anisearch)\n` +
-`12. Fixed (.filters) for admins\n`
+const eng = `● *UPDATE LIST (29/09/2021)* ●\n` + 
+`● *Büññy®Bot Version: 1.2.0v* ●\n\n` +
+`*1.* Added (.compli)\n\n` +
+`*2.* Added (.insult)\n\n` +
+`*3.* Improved (.movie)\n\n` +
+`*4.* Bot admins are fixed. (.botmin) *If you want to be a bot admin pay 1 Million $ to existing Admins.*\n\n` +
+`*5.* Removed (.currency) its buggy\n\n` +
+`*6.* Introduced (.news)\n\n` +
+`*7.* Added (.gaymeter)\n\n` +
+`*8.* Fixed (.report) Enjoy reporting\n\n` +
+`*9.* Inroduced (.device)\n\n` +
+`*10.* Sleep mode enhanced for admins to control NSFW\n\n` +
+`*11.* Introduced (.anisearch)\n\n` +
+`*12.* Fixed (.filters) for admins\n`
 
       
 
