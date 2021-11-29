a1ba986b9cf365e1581758cf2a4e198118c3faed
debojit16mitra <33363581+debojit16mitra@users.noreply.github.com>
2021-11-29 20:17:24 +0530

    Update gmeter.js

--- plugins/gmeter.js
+++ plugins/gmeter.js
@@ -1,4 +1,7 @@
-
+/* Copyright (C) 2021 BunnyBot.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+*/
 
 const Asena = require('../events');
 const con = require('../config');
