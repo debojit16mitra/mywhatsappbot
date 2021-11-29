d145a58e54b7fa7f93a82fa5eabe4f158e7f0fd5
debojit16mitra <33363581+debojit16mitra@users.noreply.github.com>
2021-11-29 19:04:08 +0530

    Update Image.js

--- queenamdi/Image.js
+++ queenamdi/Image.js
@@ -1,9 +1,7 @@
-/* Copyright (C) 2020 Black Amda.
+/* Copyright (C) 2020 BunnyBot.
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-
-QueenAmdi - Black Amda
 */
 
 const {MessageType, Presence, MessageOptions} = require('@adiwajshing/baileys');
