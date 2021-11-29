896e3ef6982e6580bf6a5472721c0c6c48eec832
debojit16mitra <33363581+debojit16mitra@users.noreply.github.com>
2021-11-29 19:46:07 +0530

    Update StringSession.js

--- queenamdi/StringSession.js
+++ queenamdi/StringSession.js
@@ -1,9 +1,8 @@
-/* Copyright (C) 2020 Black Amda.
+/* Copyright (C) 2020 BunnyBot.
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-QueenAmdi - Black Amda
 */
 
 const fs = require('fs');
