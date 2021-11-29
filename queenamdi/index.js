891babfedf3dee63fa6bca1499c805051592ee59
debojit16mitra <33363581+debojit16mitra@users.noreply.github.com>
2021-11-29 19:47:39 +0530

    Update index.js

--- queenamdi/index.js
+++ queenamdi/index.js
@@ -1,9 +1,6 @@
-/* Copyright (C) 2020 Black Amda.
-
+/* Copyright (C) 2020 BunnyBot.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-
-QueenAmdi - Black Amda
 */
 
 module.exports = {
