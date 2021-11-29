9a13b234cdd9325ce2e5cb3fd195a7c0958a4ae2
debojit16mitra <33363581+debojit16mitra@users.noreply.github.com>
2021-11-29 19:54:23 +0530

    Add files via upload

---
+++ bunnybot/index.js
@@ -0,0 +1,13 @@
+/* Copyright (C) 2020 BunnyBot.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+*/
+
+module.exports = {
+    Base: require('./Base'),
+    Message: require('./Message'),
+    StringSession: require('./StringSession'),
+    ReplyMessage: require('./ReplyMessage'),
+    Image: require('./Image'),
+    Video: require('./Video')
+};
