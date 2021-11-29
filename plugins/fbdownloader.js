423ee388a7e5c8ecea247dffc3576ed27d5e7e12
debojit16mitra <33363581+debojit16mitra@users.noreply.github.com>
2021-11-29 20:10:59 +0530

    Update fbdownloader.js

--- plugins/fbdownloader.js
+++ plugins/fbdownloader.js
@@ -1,4 +1,9 @@
 
+/* Copyright (C) 2021 BunnyBot.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+*/
+
 const Bunny = require('../events')
 const BunnyApi = require('queenamdi-public-2');
 const { MessageType, Mimetype } = require('@adiwajshing/baileys')
