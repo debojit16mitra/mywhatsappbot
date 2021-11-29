ece4705be2c72720f54eaafb6325763899859e50
debojit16mitra <33363581+debojit16mitra@users.noreply.github.com>
2021-11-29 20:23:37 +0530

    Update insult.js

--- plugins/insult.js
+++ plugins/insult.js
@@ -1,3 +1,8 @@
+/* Copyright (C) 2021 BunnyBot.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+*/
+
 const Bunny = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 const got = require('got');
