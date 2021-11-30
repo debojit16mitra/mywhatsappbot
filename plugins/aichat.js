994fab95f1dbbc16f94fa4f059b25d636fb23b3e
debojit16mitra <33363581+debojit16mitra@users.noreply.github.com>
2021-11-30 19:50:52 +0530

    Update aichat.js

--- plugins/aichat.js
+++ plugins/aichat.js
@@ -45,17 +45,6 @@ if (Config.CHAT_BOT == 'true') {
 				return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDAC, MessageType.text);
 			}
 		});
-		Bunny.applyCMD({pattern: 'bot hii ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, async (message, match) => {
-			if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
-			const url = `https://api.simsimi.net/v2/?text=${match[1]}&lc=en&cf=true`;
-			try {
-				const response = await got(url);
-				const json = JSON.parse(response.body);
-			if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '\n*👸🏻 ' + Lang.BOT +'* ```What you want??```\n\n' , MessageType.text,{quoted: message.data});
-			} catch {
-				return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDAC, MessageType.text);
-			}
-		});
 		Bunny.applyCMD({pattern: 'bot hi ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, async (message, match) => {
 			if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 			const url = `https://api.simsimi.net/v2/?text=${match[1]}&lc=en&cf=true`;
