9c0cb67a752ddc8e27d757dabed11ca66044e8d9
debojit16mitra <33363581+debojit16mitra@users.noreply.github.com>
2021-11-30 19:25:06 +0530

    Update aichat.js

--- plugins/aichat.js
+++ plugins/aichat.js
@@ -45,17 +45,25 @@ if (Config.CHAT_BOT == 'true') {
 				return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDAC, MessageType.text);
 			}
 		});
-		Bunny.applyCMD({pattern: 'bot hi ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, async (message, match) => {
+		Bunny.applyCMD({pattern: 'bot hii ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, async (message, match) => {
 			if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
-		 	return await message.client.sendMessage(message.jid, '\n*👸🏻 ' + Lang.BOT +'* ```What do you want??```\n\n' , MessageType.text,{quoted: message.data});
-			catch {
+			const url = `https://api.simsimi.net/v2/?text=${match[1]}&lc=en&cf=true`;
+			try {
+				const response = await got(url);
+				const json = JSON.parse(response.body);
+			if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '\n*👸🏻 ' + Lang.BOT +'* ```What you want??```\n\n' , MessageType.text,{quoted: message.data});
+			} catch {
 				return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDAC, MessageType.text);
 			}
 		});
-		Bunny.applyCMD({pattern: 'bot hii ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, async (message, match) => {
+		Bunny.applyCMD({pattern: 'bot hi ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, async (message, match) => {
 			if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
-		 	return await message.client.sendMessage(message.jid, '\n*👸🏻 ' + Lang.BOT +'* ```Hii Bro```\n\n' , MessageType.text,{quoted: message.data});
-			catch {
+			const url = `https://api.simsimi.net/v2/?text=${match[1]}&lc=en&cf=true`;
+			try {
+				const response = await got(url);
+				const json = JSON.parse(response.body);
+			if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '\n*👸🏻 ' + Lang.BOT +'* ```Hii Bro```\n\n' , MessageType.text,{quoted: message.data});
+			} catch {
 				return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDAC, MessageType.text);
 			}
 		});
