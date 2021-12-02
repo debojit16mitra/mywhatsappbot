fd006a0c2d5234e352ed42f3eb8a704a9d46af13
debojit16mitra <33363581+debojit16mitra@users.noreply.github.com>
2021-12-02 19:16:36 +0530

    Update deviceinfo.js

--- plugins/deviceinfo.js
+++ plugins/deviceinfo.js
@@ -4,7 +4,7 @@ you may not use this file except in compliance with the License.
 */
 
 const Asena = require('../events');
-const { MessageType } = require('@adiwajshing/baileys');
+const { MessageType, Mimetype} = require('@adiwajshing/baileys');
 const got = require('got');
 const Config = require('../config');
 const axios = require('axios');
