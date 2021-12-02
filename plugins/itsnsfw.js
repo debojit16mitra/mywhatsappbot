8ed4689fe79e8ad5d846fbb7c875581469a56cdb
debojit16mitra <33363581+debojit16mitra@users.noreply.github.com>
2021-12-02 23:55:32 +0530

    Update itsnsfw.js

--- plugins/itsnsfw.js
+++ plugins/itsnsfw.js
@@ -52,7 +52,7 @@ var load = await message.client.sendMessage(message.jid,Lang2.GET_MODD,MessageTy
         ini_txt += `*📁 Video Download Link :* ${get_result.video}\n\n`
 
 	var webimage = await axios.get(get_thumb, {responseType: 'arraybuffer'})
-       await message.sendMessage(Buffer.from(webimage.data), MessageType.image, { mimetype: Mimetype.jpg, quoted: message.data, caption: '\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n*❖ Büññy®Bot YT Shorts Engine ❖*\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n```Note: 1. click (read more) then only click on download link or else link will be broken.\n2. After download change file format to .mp4```\n\n' + ini_txt})
+       await message.sendMessage(Buffer.from(webimage.data), MessageType.image, { mimetype: Mimetype.jpg, quoted: message.data, caption: '\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n*❖ Büññy®Bot YT Shorts Engine ❖*\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n*Note:*\n 1. Click (read more) then only click on download link or else link will be broken.\n2. After download change file format to .mp4\n\n' + ini_txt})
  	
 	//await message.client.sendMessage(message.jid, '\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n*❖ Büññy®Bot YT Shorts Engine ❖*\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n' + ini_txt,MessageType.text, {quoted: message.data});
 	//return await message.client.deleteMessage(message.jid, {id: load.key.id, remoteJid: message.jid, fromMe: true})
@@ -76,7 +76,7 @@ var load = await message.client.sendMessage(message.jid,Lang2.GET_MODD,MessageTy
 
 
 	var webimage = await axios.get(get_result.url, {responseType: 'arraybuffer'})
-       await message.sendMessage(Buffer.from(webimage.data), MessageType.video, { mimetype: Mimetype.mp4, quoted: message.data, caption: '\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n*❖ Büññy®Bot YT Shorts Engine ❖*\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n```Note: 1. click (read more) then only click on download link or else link will be broken.```\n\n' + ini_txt})
+       await message.sendMessage(Buffer.from(webimage.data), MessageType.video, { mimetype: Mimetype.mp4, quoted: message.data, caption: '\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n*❖ Büññy®Bot YT Shorts Engine ❖*\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n*Note:*\n 1. click (read more) then only click on download link or else link will be broken.\n\n' + ini_txt})
  	
 	//await message.client.sendMessage(message.jid, '\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n*❖ Büññy®Bot YT Shorts Engine ❖*\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n' + ini_txt,MessageType.text, {quoted: message.data});
 	//return await message.client.deleteMessage(message.jid, {id: load.key.id, remoteJid: message.jid, fromMe: true})
