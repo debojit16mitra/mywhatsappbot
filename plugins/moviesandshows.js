8a59cf68cfaa3a39ae213c416d1b912c25a28c3f
debojit16mitra <33363581+debojit16mitra@users.noreply.github.com>
2021-12-02 19:01:55 +0530

    Update moviesandshows.js

--- plugins/moviesandshows.js
+++ plugins/moviesandshows.js
@@ -14,10 +14,7 @@ const { errorMessage, infoMessage } = require('../helpers');
 
 const Language = require('../language');
 const Lang = Language.getString('scrapers');
-//----------------------------------Pro Things----------------------------------------
-const { fetchJson, getBuffer } = require('./fetcher')
-const Lang2 = Language.getString('search')
-//------------------------------------------------------------------------------------
+
 
 
 
@@ -128,24 +125,3 @@ Asena.addCommand({ pattern: 'anisearch ?(.*)', fromMe: false , desc: Lang.SHOW_D
   },
 )
 
-//----------------------------------Pro Things----------------------------------------
-
-Bunny.addCommand({ pattern: 'tiktp ?(.*)', fromMe: false, desc: Lang2.APK_DESC,  deleteCommand: false }, async (message, match) => {
-
-  var load = await message.client.sendMessage(message.jid,Lang2.GET_MODD,MessageType.text, {quoted: message.data});
-
-  get_result = await fetchJson('https://zenzapi.xyz/api/tikporn?apikey=7848cd94229e')	
-    get_status = get_result.status
-  get_result = get_result.result
-    ini_txt = ""
-	ini_txt += `📚 API Status : ${get_status}\n`
-        ini_txt += `✍ Name : ${get_result.title}\n`
-        ini_txt += `📃 Description : ${get_result.desc}\n`
-        ini_txt += `📆 Upload Date : ${get_result.upload}\n`
-	ini_txt += `🌐 Watch Online : ${get_result.source}\n`
-        ini_txt += `📁 Download Link : ${get_result.video}\n\n`
-
-
-  await message.client.sendMessage(message.jid, '*❖ Büññy®Bot NSFW Engine ❖*\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n' + ini_txt,MessageType.text, {quoted: message.data});
-  return await message.client.deleteMessage(message.jid, {id: load.key.id, remoteJid: message.jid, fromMe: true})
-})
