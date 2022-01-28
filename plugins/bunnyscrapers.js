eabfb01f44b125b553dc8980b2ac67c6dcc9b729
debojit16mitra <33363581+debojit16mitra@users.noreply.github.com>
2022-01-28 13:39:09 +0530

    Update bunnyscrapers.js

--- plugins/bunnyscrapers.js
+++ plugins/bunnyscrapers.js
@@ -4,7 +4,7 @@ you may not use this file except in compliance with the License.
 */
 
 const Amdi = require('../events');
-const QueenAmdi = require('queenamdi-public');
+const QueenAmdi = require('queenamdi-public-2');
 const {MessageType,Mimetype} = require('@adiwajshing/baileys');
 const translatte = require('translatte');
 const Config = require('../config');
