bb345ac68c4a4267a39e609d10de751ba673ccae
debojit16mitra <33363581+debojit16mitra@users.noreply.github.com>
2022-01-28 13:37:17 +0530

    Update apitools.js

--- plugins/apitools.js
+++ plugins/apitools.js
@@ -1,6 +1,6 @@
 
 const Bunny = require('../events');
-const QueenAmdi = require('queenamdi-public');
+const QueenAmdi = require('queenamdi-public-2');
 const { MessageType, Mimetype} = require('@adiwajshing/baileys');
 const got = require('got');
 const Config = require('../config');
