05a7509c33bb2fad78785c752db872879bfbeb4b
debojit16mitra <33363581+debojit16mitra@users.noreply.github.com>
2022-01-31 17:08:39 +0530

    Create _search.js

---
+++ plugins/_search.js
@@ -0,0 +1,27 @@
+const bunny = require('../events');
+const Config = require('../config');
+const {MessageType} = require('@adiwajshing/baileys');
+
+const Language = require('../language');
+const Lang = Language.getString('scrapers');
+
+let hel = '⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍\n' + '```⚕️ Büññy®Bot Search Panel ⚕️```\n' + '⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍\n';
+hel += '*🤖Command:* ```.movie```\n*📙Description:*' + ' ```Gives you Details about the Searched Movie.```\n\n';
+hel += '*🤖Command:* ```.anisearch```\n*📙Description:*' + ' ```Gives you Details about the Searched Anime/Tv Series.```\n\n';
+hel += '*🤖Command:* ```.news```\n*📙Description:*' + ' ```Gives you Latest News. Eg: .news sports```\n\n';
+
+
+
+if (Config.LANG == 'EN') {
+    if (Config.WORKTYPE == 'private') {
+        bunny.applyCMD({pattern: 'extra', fromMe: true,  deleteCommand: false, desc: Lang.ANIMEALL_DESC}, (async (message, match) => {    
+            await message.client.sendMessage(message.jid,hel, MessageType.text,{quoted: message.data});
+        }));
+    }
+
+    else if (Config.WORKTYPE == 'public') {
+        bunny.applyCMD({pattern: 'extra', fromMe: false, desc: Lang.ANIMEALL_DESC}, (async (message, match) => {    
+            await message.client.sendMessage(message.jid,hel, MessageType.text,{quoted: message.data});
+        }));
+    }
+}
