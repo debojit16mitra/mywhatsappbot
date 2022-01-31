02ce787aadb7386537f15c42d60cd38f3fadd1b6
debojit16mitra <33363581+debojit16mitra@users.noreply.github.com>
2022-01-31 16:05:41 +0530

    Update _extracommands.js

--- plugins/_extracommands.js
+++ plugins/_extracommands.js
@@ -5,28 +5,28 @@ const {MessageType} = require('@adiwajshing/baileys');
 const Language = require('../language');
 const Lang = Language.getString('scrapers');
 
-let eng = '⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍\n' + ' ```⚕️ Büññy®Bot Movie & Series Panel ⚕️```\n' + '⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎\n';
-eng += '*🤖Command:* ```.gdrive```\n*📙Description:*' + '```Google Drive Limit Bypass```\n\n';
-eng += '*🤖Command:* ```.gdrive```\n*📙Description:*' + '```Google Drive Limit Bypass```\n\n';
-eng += '*🤖Command:* ```.gdrive```\n*📙Description:*' + '```Google Drive Limit Bypass```\n\n';
-eng += '*🤖Command:* ```.gdrive```\n*📙Description:*' + '```Google Drive Limit Bypass```\n\n';
-eng += '*🤖Command:* ```.gdrive```\n*📙Description:*' + '```Google Drive Limit Bypass```\n\n';
-eng += '*🤖Command:* ```.gdrive```\n*📙Description:*' + '```Google Drive Limit Bypass```\n\n';
-eng += '*🤖Command:* ```.gdrive```\n*📙Description:*' + '```Google Drive Limit Bypass```\n\n';
-eng += '*🤖Command:* ```.gdrive```\n*📙Description:*' + '```Google Drive Limit Bypass```\n\n';
-eng += '*🤖Command:* ```.gdrive```\n*📙Description:*' + '```Google Drive Limit Bypass```\n\n';
+let hel = '⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍\n' + '```⚕️ Büññy®Bot Help Panel ⚕️```\n' + '⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍\n';
+hel += '*🤖Command:* ```.gdrive```\n*📙Description:*' + '```Google Drive Limit Bypass```\n\n';
+hel += '*🤖Command:* ```.gdrive```\n*📙Description:*' + '```Google Drive Limit Bypass```\n\n';
+hel += '*🤖Command:* ```.gdrive```\n*📙Description:*' + '```Google Drive Limit Bypass```\n\n';
+hel += '*🤖Command:* ```.gdrive```\n*📙Description:*' + '```Google Drive Limit Bypass```\n\n';
+hel += '*🤖Command:* ```.gdrive```\n*📙Description:*' + '```Google Drive Limit Bypass```\n\n';
+hel += '*🤖Command:* ```.gdrive```\n*📙Description:*' + '```Google Drive Limit Bypass```\n\n';
+hel += '*🤖Command:* ```.gdrive```\n*📙Description:*' + '```Google Drive Limit Bypass```\n\n';
+hel += '*🤖Command:* ```.gdrive```\n*📙Description:*' + '```Google Drive Limit Bypass```\n\n';
+hel += '*🤖Command:* ```.gdrive```\n*📙Description:*' + '```Google Drive Limit Bypass```\n\n';
 
 
 if (Config.LANG == 'EN') {
     if (Config.WORKTYPE == 'private') {
         bunny.applyCMD({pattern: 'extra', fromMe: true,  deleteCommand: false, desc: Lang.ANIMEALL_DESC}, (async (message, match) => {    
-            await message.client.sendMessage(message.jid,eng, MessageType.text,{quoted: message.data});
+            await message.client.sendMessage(message.jid,hel, MessageType.text,{quoted: message.data});
         }));
     }
 
     else if (Config.WORKTYPE == 'public') {
         bunny.applyCMD({pattern: 'extra', fromMe: false, desc: Lang.ANIMEALL_DESC}, (async (message, match) => {    
-            await message.client.sendMessage(message.jid,eng, MessageType.text,{quoted: message.data});
+            await message.client.sendMessage(message.jid,hel, MessageType.text,{quoted: message.data});
         }));
     }
 }
