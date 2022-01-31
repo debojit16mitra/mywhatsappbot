b1c67c68649f2d49079165fe42f1a52fa00bc953
debojit16mitra <33363581+debojit16mitra@users.noreply.github.com>
2022-01-31 14:56:27 +0530

    Create _extracommands.js

---
+++ plugins/_extracommands.js
@@ -0,0 +1,26 @@
+const bunny = require('../events');
+const Config = require('../config');
+const {MessageType} = require('@adiwajshing/baileys');
+
+const Language = require('../language');
+const Lang = Language.getString('scrapers');
+
+const eng = `⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍\n```⚕️Büññy®Bot Help Panel⚕️```\n⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍\n\n` + 
+`*🤖Command:* ```.gdrive```\n*📙Description:* ```Random Anime Images.```\n\n` +
+`*🤖Command:* ```.ranimel```\n*📙Description:* ```Random Anime Images.```\n\n` +
+`*🤖Command:* ```.nsall```\n*📙Description:* ```Get random NSFW All Types (any) images.```\n\n`
+
+if (Config.LANG == 'EN') {
+    if (Config.WORKTYPE == 'private') {
+        bunny.applyCMD({pattern: 'extra', fromMe: true,  deleteCommand: false, desc: Lang.ANIMEALL_DESC}, (async (message, match) => {    
+            await message.client.sendMessage(message.jid,eng, MessageType.text,{quoted: message.data});
+        }));
+    }
+
+    else if (Config.WORKTYPE == 'public') {
+        bunny.applyCMD({pattern: 'extra', fromMe: false, desc: Lang.ANIMEALL_DESC}, (async (message, match) => {    
+            await message.client.sendMessage(message.jid,eng, MessageType.text,{quoted: message.data});
+        }));
+    }
+}
+
