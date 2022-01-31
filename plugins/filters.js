8e76448eb983732f531998dc796d50d4acd7d3f0
debojit16mitra <33363581+debojit16mitra@users.noreply.github.com>
2022-01-31 13:08:08 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -4,14 +4,14 @@ Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 */
 
-const Asena = require('../events');
+const bunny = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 const FilterDb = require('./sql/filters');
 
 const Language = require('../language');
 const Lang = Language.getString('filters');
 
-Asena.addCommand({pattern: 'filter ?(.*)', fromMe: true, desc: Lang.FILTER_DESC, usage: '.filter "merhaba" "merhaba, nasılsın?"'}, (async (message, match) => {
+bunny.addCommand({pattern: 'filter ?(.*)', fromMe: true, desc: Lang.FILTER_DESC, usage: '.filter "merhaba" "merhaba, nasılsın?"'}, (async (message, match) => {
     match = match[1].match(/[\'\"\“](.*?)[\'\"\“]/gsm);
 
     if (match === null) {
@@ -32,7 +32,7 @@ Asena.addCommand({pattern: 'filter ?(.*)', fromMe: true, desc: Lang.FILTER_DESC,
     }
 }));
 
-Asena.addCommand({pattern: 'stop ?(.*)', fromMe: true, desc: Lang.STOP_DESC, usage: '.stop "hello"'}, (async (message, match) => {
+bunny.addCommand({pattern: 'stop ?(.*)', fromMe: true, desc: Lang.STOP_DESC, usage: '.stop "hello"'}, (async (message, match) => {
     match = match[1].match(/[\'\"\“](.*?)[\'\"\“]/gsm);
     if (match === null) {
         return await message.client.sendMessage(message.jid,Lang.NEED_REPLY + '\n*Example:* ```.stop "hello"```',MessageType.text)
@@ -48,7 +48,7 @@ Asena.addCommand({pattern: 'stop ?(.*)', fromMe: true, desc: Lang.STOP_DESC, usa
 }));
 
 
-Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
+bunny.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
     var filtreler = await FilterDb.getFilter(message.jid);
     if (!filtreler) return; 
     filtreler.map(
